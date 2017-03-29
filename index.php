944cdd4b1da7b903c131c6538d773eccc506e2d3
thomasgriffinx <xytomg@gmail.com>
2017-03-29 12:05:17 +0100

    push

--- index.php
+++ index.php
@@ -1,47 +1,59 @@
 <!DOCTYPE html>  
 <html>   
-<head>  
-<meta charset=utf-8 />  
-<title>Flooring Supply Centre - Flooring Estimate</title>  
-<style type="text/css">  
-body {margin: 30px;}  
-</style>   
-</head>  
-<body>  
-<h2> Enter Room Dimensions</h2>
-<form>  
-Length: <input type="text" id="firstNumber" /><br>  
-Width: <input type="text" id="secondNumber" /><br>  
-<input type="button" onClick="multiplyBy()" Value="Calculate" />  
-</form>  
-<p>Total floor required in sqm:<br>  
-<span id = "result"></span>  
-</p>   
-<form>
+		<head>  
+		<meta charset=utf-8 />  
+		<title>Flooring Supply Centre - Flooring Estimate</title>  
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/css/form-elements.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+        <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+		<style type="text/css">  
+		body {margin: 30px;}  
+		</style>   
+		</head>  
+	<body>  
+	<h2>Enter Room Dimensions</h2>
+		<form>  
+			Length: <input type="text" id="firstNumber" /><br>  
+			Width: <input type="text" id="secondNumber" /><br>  
+			<input type="button" onClick="multiplyBy()" Value="Calculate" />  
+		</form>  
+			<p>Total floor required in sqm:<br>  
+			<span id = "result"></span>  
+			</p>   
+			
+			<form>
+				<H2> Divide your room size by pack coverage </H2>  
+				Room Size(sqm): <input type="text" id="thirdNumber" /><br>  
+				Pack coverage: <input type="text" id="fourthNumber" /><br>   
+				<input type="button" onClick="divideBy()" Value="Calculate" />  
+			</form>  
+				<p>Total packs required: <br>  
+				<span id = "result2"></span>  
+				</p>
 
-<H2> Divide your room size by pack coverage </H2>  
-Room Size(sqm): <input type="text" id="thirdNumber" /><br>  
-Pack coverage: <input type="text" id="fourthNumber" /><br>   
-<input type="button" onClick="divideBy()" Value="Calculate" />  
-</form>  
-<p>Total packs required: <br>  
-<span id = "result2"></span>  
-</p>
+										<script type="text/javascript">
+												function multiplyBy()  
+										{  
+										        num1 = document.getElementById("firstNumber").value;  
+										        num2 = document.getElementById("secondNumber").value;  
+										        document.getElementById("result").innerHTML = num1 * num2;  
+										}  
+										  
+										function divideBy()   
+										{   
+										        num3 = document.getElementById("thirdNumber").value;  
+										        num4 = document.getElementById("fourthNumber").value;  
+										document.getElementById("result2").innerHTML = num3 / num4;  
+										} </script>	
+										 	<script src="assets/js/jquery-1.11.1.min.js"></script>
+									        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+									        <script src="assets/js/jquery.backstretch.min.js"></script>
+									        <script src="assets/js/scripts.js"></script>
 
-<script type="text/javascript">
-		function multiplyBy()  
-{  
-        num1 = document.getElementById("firstNumber").value;  
-        num2 = document.getElementById("secondNumber").value;  
-        document.getElementById("result").innerHTML = num1 * num2;  
-}  
-  
-function divideBy()   
-{   
-        num3 = document.getElementById("thirdNumber").value;  
-        num4 = document.getElementById("fourthNumber").value;  
-document.getElementById("result2").innerHTML = num3 / num4;  
-} </script>	
-
-	</body>
-</html>
+											</body>
+										</html>
