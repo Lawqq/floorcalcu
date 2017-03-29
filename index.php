9c257590e21c1d74a1ba2dd5882a862fffeb8550
thomasgriffinx <xytomg@gmail.com>
2017-03-29 11:53:56 +0100

    push

--- index.php
+++ index.php
@@ -2,26 +2,25 @@
 <html>   
 <head>  
 <meta charset=utf-8 />  
-<title>Flooring Supply Centre</title>  
+<title>Flooring Supply Centre - Flooring Estimate</title>  
 <style type="text/css">  
 body {margin: 30px;}  
 </style>   
 </head>  
 <body>  
-<h1> Enter Room Dimensions <h1>
+<h2> Enter Room Dimensions</h2>
 <form>  
 Length: <input type="text" id="firstNumber" /><br>  
 Width: <input type="text" id="secondNumber" /><br>  
 <input type="button" onClick="multiplyBy()" Value="Calculate" />  
 </form>  
-<p>Total floor required in sqm: <br>  
+<p>Total floor required in sqm:<br>  
 <span id = "result"></span>  
 </p>   
-
 <form>
 
-<H1> Divide your room size by pack coverage <H1>  
-Room Size: <input type="text" id="thirdNumber" /><br>  
+<H2> Divide your room size by pack coverage </H2>  
+Room Size(sqm): <input type="text" id="thirdNumber" /><br>  
 Pack coverage: <input type="text" id="fourthNumber" /><br>   
 <input type="button" onClick="divideBy()" Value="Calculate" />  
 </form>  
